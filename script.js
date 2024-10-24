90acf18a3a8cb97e930cbbda7c11a1299b0eb8c6
AzkaBeker7947 <144089670+AzkaBeker7947@users.noreply.github.com>
2024-10-24 16:25:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    const expression = display.value;
+    
+    if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
+        display.value = 'i love You XD';
+        setTimeout(function() {
+            display.value = 'btw coba pencet "=" lagi :D'
+        }, 3000);
+    } else {
+        try {
+            display.value = eval(expression);
+        } catch {
+            window.location.href = "flower tree.html";
+        }
+    }
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
